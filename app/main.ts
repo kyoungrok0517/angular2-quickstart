3df998d14fecc5035f1d1b9b50cffff03ae78002
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-04-18 11:13:48 +0900

    init

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+// Note that different bootstrap method
+// is required depending on the running platform
+bootstrap(AppComponent);
